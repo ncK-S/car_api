a7d59b22cb7ef7d92ef0df080328d020cbd9dacd
Nick Stein <n.cstein@outlook.com>
2022-02-08 07:28:39 -0500

    starter_code

---
+++ car_collectors/site/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template
+
+site = Blueprint('site', __name__, template_folder='site_templates')
+
+''' note that in the above code some arguments are specified 
+the blueprint object the first argument sit is the blueprint's name'''
+
+@site.route('/')
+def home():
+    return render_template('index.html')
+
+@site.route('/about')
+def about():
+    return render_template('about.html')
